ee25262a1d9afde3ff70e72e235b7cb0831501de
tyahlego <tyazhlego@gmail.com>
2020-11-15 19:15:49 +0300

    final version

--- api.py
+++ api.py
@@ -1,4 +1,12 @@
+if __name__ == '__main__':
+    pass
+
+
 import os
+import logging
+import time
+import requests
+
 
 from twilio.rest import Client
 from dotenv import load_dotenv
@@ -9,10 +17,33 @@ TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
-message = client.messages.create(
-    body='ПРИВЕТ!!!',  # текст сообщения
-    from_='19378216672',  # номер, который был получен
-    to='79951123345',  # твой номер, на который придёт sms
-    )
+logging.basicConfig(filename='logg.log', level=logging.INFO)
 
-print(message.sid)
+def api_check():
+    while True:
+        try:
+            response = requests.get('http://ya.ru/')
+            logging.info('запрос к серверу....')
+        except:
+            logging.warning('ошибка!')
+            client.messages.create(
+                body='Ошибка в мониторинге',  # текст сообщения
+                from_='19378216672',  # номер, который был получен
+                to='79951123345',  # твой номер, на который придёт sms
+                )
+            time.sleep(6)
+            exit
+        else:
+            if response.status_code == 200:
+                logging.info('всё хорошо')
+            else:
+                logging.error('все сломалось!')
+                client.messages.create(
+                body='Ошибка в мониторинге',  # текст сообщения
+                from_='19378216672',  # номер, который был получен
+                to='79951123345',  # твой номер, на который придёт sms
+                )
+            time.sleep(6)
+
+
+api_check()
